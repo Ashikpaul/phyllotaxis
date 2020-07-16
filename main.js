11fded2535401a1a78370ca6ffe2ce6520b14f58
Ashik Paul <ashik.paul@sacha.solutions>
2020-07-17 00:41:47 +0530

    added text

--- main.js
+++ main.js
@@ -1,72 +1,6 @@
-// const canvas = document.getElementById("flower");
-// const ctx = canvas.getContext("2d");
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-// var a = 0,n = 0,r = 0,c = 6;
-// var x = 0, y = 0;
-// var w = window.innerWidth, h = window.innerHeight;
-// var cx = w/2, cy = h/2;
-// var rot = 1;
-
-// //draw stem
-// ctx.beginPath();
-// ctx.moveTo(cx, h);
-// ctx.lineWidth = 10;
-// ctx.strokeStyle = "darkgreen";
-// ctx.quadraticCurveTo(cx+250, cy+100, cx, cy);
-// ctx.stroke();
-// ctx.lineWidth = 1;
-
-// //start rotating
-// const startRotate = () =>{
-//   setInterval(function () {
-//     ctx.save(); //saves the state of canvas
-//     ctx.translate(cx, cy); //let's translate
-//     ctx.rotate(Math.PI / 180 * (rot += 5)); //increment the angle and rotate the image 
-//     ctx.restore(); //restore the state of canvas
-//   }, 100);
-// }
-
-// const drawFrame = () => {
-
-// }
-
-// const startDrawing = () =>{
-//   //draw bud
-//   if(n < 100){
-//     a =  (n * 137.5)*(180/Math.PI);
-//     r = c * Math.sqrt(n);
-//     x = r * Math.cos(a*(180/Math.PI)) + cx;
-//     y = r * Math.sin(a*(180/Math.PI)) + cy;
-//     ctx.beginPath();
-//     ctx.arc(x, y, 6, 0, 2 * Math.PI, true);
-//     ctx.strokeStyle = "lime";
-//     ctx.fillStyle = 'yellow';
-//     ctx.fill();
-//     ctx.stroke();
-//   }else{
-//     //draw petals
-//     a =  (n * 137.5)*(180/Math.PI);
-//     r = c * Math.sqrt(n);
-//     x = r * Math.cos(a*(180/Math.PI)) + cx;
-//     y = r * Math.sin(a*(180/Math.PI)) + cy;
-//     ctx.beginPath();
-//     ctx.arc(x, y, 6, 0, 2 * Math.PI, true);
-//     ctx.strokeStyle = "black";
-//     ctx.fillStyle = 'hsl('+ a%256 +',100%,50%)';
-//     ctx.fill();
-//     ctx.stroke();
-//   }
-//   n++;
-//   if(n < 1000)
-//     requestAnimationFrame(startDrawing);
-//   else
-//     startRotate();
-// }
-// startDrawing();
-
 const canvas = document.getElementById("flower");
 const ctx = canvas.getContext("2d");
+const blah = canvas.getContext("2d");
 var w = canvas.width = window.innerWidth;
 var h = canvas.height = window.innerHeight;
 var circles = [];
@@ -90,25 +24,47 @@ class circle {
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.lineWidth = 1;
     ctx.beginPath();
     ctx.arc(this.x, this.y, 6, 0, 2 * Math.PI);
     ctx.strokeStyle = this.strokeStyle;
     ctx.fillStyle = this.fillStyle;
     ctx.fill();
     ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
   }
 }
 
 const startRotate = () => {  
-  ctx.clearRect(-w, -h, w*2, h*2);    
-  ctx.rotate(0.9);
+  ctx.clearRect(-w/2, -h/2, w*2, h*2);   
+  ctx.rotate(0.5);
   circles.forEach(c => { c.draw(ctx) });
   requestAnimationFrame(startRotate);
 }
 
 const startDrawing = () => {
-  circles.push(new circle(n))
-  ctx.clearRect(-w, -h, w*2, h*2);
+  ctx.globalAlpha = 1.0;
+  ctx.textAlign = "center";
+  ctx.font = "30px monospace";
+  ctx.fillStyle = "yellow";
+  ctx.fillText('Phyllotaxis', 0, -380);
+  ctx.fillStyle = "red";
+  ctx.font = "20px monospace";
+  ctx.fillText("WARNING: Don't stare at it for long", 0, -340);
+  ctx.textAlign = "left";
+  ctx.fillStyle = "white";
+  ctx.font = "15px monospace";
+  ctx.fillText('By Ashik', 20, -365);
+  ctx.beginPath();
+  ctx.moveTo(0, h/2);
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "darkgreen";
+  ctx.quadraticCurveTo(250, 100, 0, 0);
+  ctx.stroke();
+  ctx.closePath();
+  circles.push(new circle(n))  
   circles.forEach(c => { c.draw(ctx) });
   n++;
   
